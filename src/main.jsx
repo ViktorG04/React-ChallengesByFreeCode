463f4b8dbec225473c733eb31536f7d268c07cb2
victor García <vic_antonio_92@hotmail.es>
2023-08-15 23:14:15 -0600

    Markdown challenge done

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,10 @@ import Random from "./pages/Random";
 import Calculator from "./pages/Calculator";
 import Drum from "./pages/Drum";
 import Clock from "./pages/Clock";
+import Markdown from "./pages/Markdown";
 import { DrumProvider } from "./context/DrumContext";
 import { TimeProvider } from "./context/TimeContext";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <TimeProvider>
-    <Clock />
-  </TimeProvider>,
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Markdown />);
